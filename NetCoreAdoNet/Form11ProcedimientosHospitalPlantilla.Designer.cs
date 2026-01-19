c0480e4ea90dc5bcad3fdf401868b0cf91397872
raulgar19 <raulgamu2003@gmail.com>
2026-01-19 10:09:20 +0100

    Add Form11 for hospital staff salary updates via SPs
    Introduces Form11ProcedimientosHospitalPlantilla, enabling users to select a hospital, increment staff salaries, and view updated staff lists using SQL Server stored procedures. Updates Program.cs to launch the new form. Adds designer and resource files for UI support.

---
+++ NetCoreAdoNet/Form11ProcedimientosHospitalPlantilla.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace NetCoreAdoNet
+{
+    partial class Form11ProcedimientosHospitalPlantilla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            cmbHospitales = new ComboBox();
+            label2 = new Label();
+            txtIncremento = new TextBox();
+            btnUpdate = new Button();
+            label3 = new Label();
+            lstPlantilla = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Hospitales";
+            // 
+            // cmbHospitales
+            // 
+            cmbHospitales.FormattingEnabled = true;
+            cmbHospitales.Location = new Point(12, 27);
+            cmbHospitales.Name = "cmbHospitales";
+            cmbHospitales.Size = new Size(121, 23);
+            cmbHospitales.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(150, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Incremento";
+            // 
+            // txtIncremento
+            // 
+            txtIncremento.Location = new Point(150, 27);
+            txtIncremento.Name = "txtIncremento";
+            txtIncremento.Size = new Size(159, 23);
+            txtIncremento.TabIndex = 3;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(113, 67);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 23);
+            btnUpdate.TabIndex = 4;
+            btnUpdate.Text = "Modificar salarios";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 116);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Plantilla";
+            // 
+            // lstPlantilla
+            // 
+            lstPlantilla.FormattingEnabled = true;
+            lstPlantilla.Location = new Point(13, 134);
+            lstPlantilla.Name = "lstPlantilla";
+            lstPlantilla.Size = new Size(296, 124);
+            lstPlantilla.TabIndex = 6;
+            // 
+            // Form11ProcedimientosHospitalPlantilla
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(325, 271);
+            Controls.Add(lstPlantilla);
+            Controls.Add(label3);
+            Controls.Add(btnUpdate);
+            Controls.Add(txtIncremento);
+            Controls.Add(label2);
+            Controls.Add(cmbHospitales);
+            Controls.Add(label1);
+            Name = "Form11ProcedimientosHospitalPlantilla";
+            Text = "Form11ProcedimientosHospitalPlantilla";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox cmbHospitales;
+        private Label label2;
+        private TextBox txtIncremento;
+        private Button btnUpdate;
+        private Label label3;
+        private ListBox lstPlantilla;
+    }
+}
---
+++ NetCoreAdoNet/Form11ProcedimientosHospitalPlantilla.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#region PROCEDIMIENTOS
+//create procedure SP_ALL_HOSPITALES
+//as
+//	select * from HOSPITAL
+//go
+
+//create procedure SP_UPDATE_PLANTILLA_HOSPITAL
+//(@nombre as nvarchar(50), @incremento int)
+//as
+//	declare @hospitalcod int
+//	select @hospitalcod = HOSPITAL_COD from HOSPITAL
+//	where NOMBRE=@nombre
+//	update PLANTILLA set SALARIO = SALARIO + @incremento
+//	where HOSPITAL_COD = @hospitalcod
+//go
+
+//create procedure SP_PLANTILLA_HOSPITAL
+//(@nombre nvarchar(50))
+//as
+//	select PLANTILLA.* from PLANTILLA
+//	inner join HOSPITAL
+//	on HOSPITAL.HOSPITAL_COD = PLANTILLA.HOSPITAL_COD
+//	where HOSPITAL.NOMBRE = @nombre
+//go
+#endregion
+
+namespace NetCoreAdoNet
+{
+    public partial class Form11ProcedimientosHospitalPlantilla : Form
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public Form11ProcedimientosHospitalPlantilla()
+        {
+            InitializeComponent();
+
+            string connectionString = "Data Source=LOCALHOST\\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+
+            this.LoadHospitales();
+        }
+
+        private async void LoadHospitales()
+        {
+            string sql = "SP_ALL_HOSPITALES";
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = sql;
+            
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            this.cmbHospitales.Items.Clear();
+
+            while (await this.reader.ReadAsync())
+            {
+                string nombre = this.reader["NOMBRE"].ToString();
+                this.cmbHospitales.Items.Add(nombre);
+            }
+
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+        }
+
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string sql = "SP_UPDATE_PLANTILLA_HOSPITAL";
+            string nombreHospital = this.cmbHospitales.SelectedItem.ToString();
+            int incremento = int.Parse(this.txtIncremento.Text);
+
+            this.com.Parameters.AddWithValue("@nombre", nombreHospital);
+            this.com.Parameters.AddWithValue("@incremento", incremento);
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = sql;
+
+            await this.cn.OpenAsync();
+            int registros = await this.com.ExecuteNonQueryAsync();
+
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+
+            await this.LoadPlantillaHospital(nombreHospital);
+
+            MessageBox.Show("Registros modificados: " + registros);
+        }
+
+        private async Task LoadPlantillaHospital(string nombreHospital)
+        {
+            string sql = "SP_PLANTILLA_HOSPITAL";
+
+            this.com.Parameters.AddWithValue("@nombre", nombreHospital);
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = sql;
+
+            await this.cn.OpenAsync();
+
+            this.reader = await this.com.ExecuteReaderAsync();
+            
+            this.lstPlantilla.Items.Clear();
+
+            while (await this.reader.ReadAsync())
+            {
+                string apellido = this.reader["APELLIDO"].ToString();
+                string salario = this.reader["SALARIO"].ToString();
+
+                this.lstPlantilla.Items.Add(apellido + " - " + salario);
+            }
+
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+        }
+    }
+}
